c3c5bae7aa94d0eff749c3139169f0dcd295b2ba
CodexX777 <shivang260279@gmail.com>
2023-04-12 04:47:48 +0530

    add some documents to the book collection using a function AddDocuments

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const Book = require("./models/bookSchema");
+
+
+
+app.use(bodyParser.json());
+
+// const AddDocuments=async()=>{
+    
+//     const books = [
+//         {
+//           title: 'The Hitchhiker\'s Guide to the Galaxy',
+//           author: 'Douglas Adams',
+//           publishedYear: 1979,
+//           genre: 'Science Fiction',
+//           pageCount: 224,
+//           price: 9.99,
+//           ratings: [4, 5, 4, 3, 5],
+//           reviews: [
+//             { username: 'john_doe', text: 'Great book!', rating: 5 },
+//             { username: 'jane_doe', text: 'A classic!', rating: 4 },
+//             { username: 'bob_smith', text: 'Meh...', rating: 3 }
+//           ]
+//         },
+//         {
+//           title: 'To Kill a Mockingbird',
+//           author: 'Harper Lee',
+//           publishedYear: 1960,
+//           genre: 'Fiction',
+//           pageCount: 281,
+//           price: 12.99,
+//           ratings: [5, 5, 4, 5],
+//           reviews: [
+//             { username: 'alice_smith', text: 'One of the best books ever written!', rating: 5 },
+//             { username: 'bob_jones', text: 'A timeless classic', rating: 5 },
+//             { username: 'jane_doe', text: 'Really enjoyed this book', rating: 4 }
+//           ]
+//         },
+//         {
+//           title: 'The Great Gatsby',
+//           author: 'F. Scott Fitzgerald',
+//           publishedYear: 1925,
+//           genre: 'Fiction',
+//           pageCount: 180,
+//           price: 7.99,
+//           ratings: [4, 3, 5, 4, 3],
+//           reviews: [
+//             { username: 'john_doe', text: 'Beautifully written', rating: 5 },
+//             { username: 'jane_doe', text: 'Overrated in my opinion', rating: 3 },
+//             { username: 'bob_smith', text: 'Not my cup of tea', rating: 2 }
+//           ]
+//         }
+//       ];
+  
+//       await Book.insertMany(books).then(()=>{
+//         console.log("Successfull document addition");
+//       });
+
+// }
+
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xvgrljc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//   AddDocuments();
+
+
+  
